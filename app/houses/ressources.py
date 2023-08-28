2c9485466c34a5aefd82766359f6f99d9720abd1
vwez <desirv63@gmail.com>
2023-08-29 01:04:12 +0200

    add endpoint to list house cities

--- app/houses/ressources.py
+++ app/houses/ressources.py
@@ -1,10 +1,11 @@
 from flask import request
 from flask.views import MethodView
-from flask_smorest import Blueprint, abort
+from flask_smorest import Blueprint, abort, requires, doc
 from injector import inject
 from flask_jwt_extended import jwt_required
 
-from schemas import HouseSchema, HouseUpdateSchema
+
+from schemas import HouseSchema, HouseUpdateSchema, HouseCitiesSchema
 from app.houses.service import HouseService
 from app.houses.models import HouseModel
 
@@ -24,17 +25,23 @@ class Category(MethodView):
         self.house_service = HouseService()
 
     @jwt_required()
+    @requires("Authorization", description="Authentication token required")
     @blp.response(200, HouseSchema)
+    @doc(tags=['Houses'], security=[{'Bearer': []}])
     def get(self, house_id:int):
         return self.house_service.get_house_by_id(house_id) 
 
     @jwt_required()
+    @requires("Authorization", description="Authentication token required")
+    @doc(tags=['Houses'], security=[{'Bearer': []}])
     def delete(self, house_id:int):
         return self.house_service.delete_house(house_id)
 
     @jwt_required(fresh=True)
+    @requires("Authorization", description="Authentication token required")
     @blp.arguments(HouseUpdateSchema) 
     @blp.response(200, HouseSchema)
+    @doc(tags=['Houses'], security=[{'Bearer': []}])
     def put(self, house_data, house_id):
         return self.house_service.update_house(house_id= house_id, house_data= house_data)
 
@@ -51,8 +58,25 @@ class CategoryList(MethodView):
         return self.house_service.get_all_houses()
 
     @jwt_required()
+    @requires("Authorization", description="Authentication token required")
     @blp.arguments(HouseSchema)
     @blp.response(201, HouseSchema)
+    @doc(tags=['Houses'], security=[{'Bearer': []}])
     def post(self, house_data):
         house = HouseModel(**house_data)
         return self.house_service.create_house(house)
+    
+@blp.route("/house/cities")
+class CityList(MethodView):
+    @inject
+    def __init__(self):
+        self.house_service = HouseService()
+
+    @blp.response(200, HouseCitiesSchema(many=True))
+    def get(self):
+
+        """
+        Returns list of city houses 
+        """
+        cities = [ house.city for house in self.house_service.get_all_houses()]
+        return cities
